a338f499c765f2bcbeb341ed9add428a5b53cd2c
Kanchan Amre <kanchan.amre@raweng.com>
2018-09-05 16:10:33 +0530

    initial commit

--- models/contentstack.js
+++ models/contentstack.js
@@ -1,8 +1,6 @@
 var Contentstack = require('contentstack')
 var config = require('config')
 
-var Stack = Contentstack.Stack(config.get('apikey'), config.get('accesstoken'), config.get('environment'))
-
-
+var Stack = Contentstack.Stack(config.get('apikey'), config.get('accesstoken'), config.util.getEnv('NODE_ENV'))
 
 module.exports = Stack
